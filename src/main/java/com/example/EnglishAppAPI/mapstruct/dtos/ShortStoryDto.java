71c95578a790bc816bc7e16f07f4865bc8c85b73
Phu Le <21522466@gm.uit.edu.vn>
2024-04-24 16:43:00 +0700

    add ShortStoryMapper

--- src/main/java/com/example/EnglishAppAPI/mapstruct/dtos/ShortStoryDto.java
+++ src/main/java/com/example/EnglishAppAPI/mapstruct/dtos/ShortStoryDto.java
@@ -1,5 +1,7 @@
 package com.example.EnglishAppAPI.mapstruct.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class ShortStoryDto {
+    @JsonProperty("title")
+    @NotEmpty(message = "this title field is required")
     private String title;
+    @JsonProperty("paragraph")
+    @NotEmpty(message = "this paragraph field is required")
     private String paragraph;
+    @JsonProperty("image")
+    @NotEmpty(message = "this image field is required")
     private String image;
 }
